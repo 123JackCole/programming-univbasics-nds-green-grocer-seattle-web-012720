820db58e33ea1fa10765a085ca69fdbb65fbe3f5
Jack Cole <123jack.cole@gmail.com>
2020-01-16 23:40:04 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -63,7 +63,7 @@ def apply_coupons(cart, coupons)
       else
         cart_item_with_coupon = {
           item: couponed_item_name,
-          price: coupons[index][:cost] / coupons,
+          price: coupons[index][:cost] / coupons[index][:num],
           count: ,
           clearance: 
         }
